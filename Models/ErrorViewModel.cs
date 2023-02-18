eee8f7e6561bfdccac9724f371073db82b69fafc
Raj Mourya <rajhmourya@gmail.com>
2023-02-18 11:39:22 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dotnet_backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
